2a407238f304a11c323be7f9793a64be41db7b19
Muhab <muhabme@gmail.com>
2023-03-07 19:24:51 +0200

    render update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import { router as authRouter } from './routes/auth.js';
+import { router as authRouter } from "./routes/auth.js";
 import { router as userRouter } from "./routes/users.js";
 import { router as postRouter } from "./routes/posts.js";
 
@@ -16,14 +16,26 @@ app.use(express.json());
 // app.use(helmet());
 // app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-// app.use(cors());
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 // ROUTES
 
 app.use("/auth", authRouter);
-app.use("/users", userRouter)
-app.use("/posts", postRouter); 
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
 
 // MONGOOSE SETUP
 const port = process.env.PORT || 6001;
@@ -36,6 +48,6 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(port, () => console.log(`Server listening on port: ${port}`));
+    app.listen(port, () => console.log(`Server is Live`));
   })
   .catch((error) => console.log(error));
